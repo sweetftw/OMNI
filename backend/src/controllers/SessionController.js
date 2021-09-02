6750ea3d74b0803843ea7d8d0889fc1626d56a0d
sweetftw <yandiaspg@gmail.com>
2021-09-02 20:16:18 -0300

    Back-end

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,19 @@
+const connection = require('../database/connection');
+const { create } = require('./IncidentController');
+
+module.exports = {
+    async create(request, response) {
+        const { id } = request.body;
+
+        const ong = await connection('ongs')
+            .where('id', id)
+            .select('name')
+            .first();
+
+        if (!ong) {
+        return response.status(400).json({ error: 'No ONG found'})
+        }
+
+        return response.json(ong);
+    }
+}
